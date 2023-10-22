df1afc390293ec3772ba1acc8397c6964d3f1345
Masaya Hayashi <mcdonalds.only@gmail.com>
2023-10-22 11:37:41 +0900

    setup pprof

--- webapp/go/cmd/isuports/main.go
+++ webapp/go/cmd/isuports/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"runtime"
+
 	isuports "github.com/isucon/isucon12-qualify/webapp/go"
 )
 
 func main() {
+	runtime.SetBlockProfileRate(1)
+	runtime.SetMutexProfileFraction(1)
+	go func() {
+		log.Fatal(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	isuports.Run()
 }
